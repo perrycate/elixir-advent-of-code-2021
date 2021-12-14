2ff7941c68b0208aa8469bcd34bb2cf3f855e3d3
Perry Cate <NPCate01@gmail.com>
2021-12-13 19:22:58 -0800

    Day 7
    20ish minutes, not bad.
    Code is ugly tho.

---
+++ 7/7.exs
@@ -0,0 +1,33 @@
+defmodule Day7 do
+  def part1 do
+    positions = parse("input")
+    p = Enum.min_by(positions, fn x -> linear_fuel_to(positions, x) end)
+    linear_fuel_to(positions, p)
+  end
+
+  def part2 do
+    positions = parse("input")
+    p = Enum.min_by(positions, fn x -> quadratic_fuel_to(positions, x) end)
+    quadratic_fuel_to(positions, p)
+  end
+
+  def linear_fuel_to(positions, target) do
+    positions
+    |> Enum.reduce(0, fn pos, sum -> sum + abs(pos - target) end)
+  end
+
+  def quadratic_fuel_to(positions, target) do
+    positions
+    |> Enum.reduce(0, fn pos, sum -> 
+      d = abs(pos - target)
+      sum + (d * (d+1) / 2)
+    end)
+  end
+
+  def parse(filename) do
+    File.read!(filename)
+    |> String.trim
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+end
