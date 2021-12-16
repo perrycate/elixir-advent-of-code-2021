e7628f9e623ecc7aa125cfd2b6fd4d82e10c3adf
Perry Cate <NPCate01@gmail.com>
2021-12-15 21:05:01 -0800

    Day 9 Part 2

--- 9/9.exs
+++ 9/9.exs
@@ -1,15 +1,45 @@
 defmodule Day9 do
   def part1 do
     depths = parse("input")
+    find_low_points(depths)
+    |> Stream.map(fn {i, j} -> 1 + get(depths, i, j) end)
+    |> Enum.sum
+  end
+
+  def part2 do
+    depths = parse("input")
+    find_low_points(depths)
+    |> Stream.map(fn coord -> explore_basin(depths, [coord], MapSet.new()) end)
+    |> Stream.uniq
+    |> Stream.map(&MapSet.size/1)
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.reduce(&*/2) # Multiply.
+  end
+
+  def explore_basin(_depths, [], visited), do: visited
+  def explore_basin(depths, [{i, j} | to_visit], visited) do
+    if MapSet.member?(visited, {i, j}) || get(depths, i, j, 9) == 9 do
+      explore_basin(depths, to_visit, visited)
+    else
+      explore_basin(
+        depths,
+        neighbors_of(i, j) ++ to_visit,
+        MapSet.put(visited, {i, j})
+      )
+    end
+  end
+
+  def find_low_points(depths) do
     for i <- 0..(tuple_size(depths)-1), j <- 0..(tuple_size(elem(depths, 0))-1) do
       {i, j}
     end
-    |> Stream.filter(fn {i, j} ->
+    |> Enum.filter(fn {i, j} ->
       v = get(depths, i, j)
-      Enum.all?(neighbors_of(depths, i, j), &(&1 > v))
+      Enum.all?(
+        neighbors_of(i, j),
+        fn {i2, j2} -> get(depths, i2, j2) > v end)
     end)
-    |> Stream.map(fn {i, j} -> 1 + get(depths, i, j) end)
-    |> Enum.sum
   end
 
   def parse(filename) do
@@ -25,16 +55,17 @@ defmodule Day9 do
     |> List.to_tuple
   end
 
-  def neighbors_of(ns, i, j) do
-    up   = if i > 0,                do: get(ns, i-1, j), else: Inf
-    down = if i < tuple_size(ns)-1, do: get(ns, i+1, j), else: Inf
+  def neighbors_of(i, j), do: [{i-1, j}, {i+1, j}, {i, j-1}, {i, j+1}]
 
-    left  = if j > 0,                do: get(ns, i, j-1), else: Inf
-    right = if j < tuple_size(elem(ns, 0))-1, do: get(ns, i, j+1), else: Inf
-
-    [up, down, left, right]
+  def get(double_tuple, i, j, default \\ Inf) do
+    if i < 0 ||
+       i > (tuple_size(double_tuple)-1) ||
+       j < 0 ||
+       j > (tuple_size(elem(double_tuple, 0))-1) do
+      default
+    else
+      elem(elem(double_tuple, i), j)
+    end
   end
-
-  def get(double_tuple, i, j), do: elem(elem(double_tuple, i), j)
 
 end
