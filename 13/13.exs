8a09b3f91423f6b74ad5c7380aad91d5611ead11
Perry Cate <NPCate01@gmail.com>
2021-12-27 23:54:07 -0500

    Day 13 Part 2

--- 13/13.exs
+++ 13/13.exs
@@ -1,5 +1,4 @@
 defmodule Day13 do 
-  
   def part1 do
     {coords, _} = parse("input")
     coords
@@ -8,18 +7,53 @@ defmodule Day13 do
     |> Enum.count
   end
 
+  def part2 do
+    {coords, instructions} = parse("input")
+
+    folded_coords = instructions
+             |> Enum.reduce(coords, fn {dir, axis}, coords ->
+               coords
+               |> Enum.map(&fold(&1, dir, axis))
+               |> MapSet.new
+             end)
+
+    # Finesse into printable format.
+    dots_per_line = folded_coords
+                    |> Enum.group_by(&elem(&1, 1), &elem(&1, 0))
+
+    for y <- 0..Enum.max(Map.keys(dots_per_line)) do
+      for x <- 0..Enum.max(dots_per_line[y]) do
+        # Cubic but idc.
+        # If we need to make it more efficient, turn each value in dots_per_line into a set.
+        if x in dots_per_line[y] do
+          IO.write "*"
+        else
+          IO.write " "
+        end
+      end
+      IO.write "\n"
+    end
+  end
+
   def parse(filename) do
-    [raw_coords, _raw_instructions] = File.read!(filename) |> String.split("\n\n")
+    [raw_coords, raw_instructions] = File.read!(filename) |> String.split("\n\n")
 
     coords = for line <- String.split(raw_coords, "\n"), into: %MapSet{} do
-               line
-               |> String.split(",")
-               |> Enum.map(&String.to_integer/1)
-               |> List.to_tuple
-             end
+      line
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+      |> List.to_tuple
+    end
 
-    # TODO(part2) parse instructions.
-    {coords, []}
+    instructions = for line <- String.split(raw_instructions, "\n", trim: true), [] do
+      line
+      |> IO.inspect
+      |> String.split("=")
+      |> then(fn [axis_str, num] ->
+        {String.at(axis_str, -1), String.to_integer(num)}
+      end)
+    end
+    {coords, instructions}
   end
 
   def fold({x, y}, "x", axis), do: {fold_val(x, axis), y}
